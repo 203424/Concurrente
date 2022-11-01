83a11ab066352aaf2a966c6fb16a56f0b6ca942d
Gabriel <gabrielramondev23@gmail.com>
2022-11-01 00:00:09 -0600

    Actividad 05-02

--- C2/Actividad 05-02.py
+++ C2/Actividad 05-02.py
@@ -1,35 +1,184 @@
+from msilib.schema import ControlEvent
 import threading
 import time
-import random
+from random import choice, randint
 
-CAPACIDAD = 100
+CAPACIDAD = 10
+COMENSALES = 15
 MESEROS = round(CAPACIDAD * 0.1)
 COCINEROS = round(CAPACIDAD * 0.1)
 RESERVACION_MAX = round(CAPACIDAD * 0.2)
 
-'''
-RECEPCIÓN
-- Los comensales llegan de forma individual o en grupo
-- La recepción solo puede atender de forma individual o en grupo
+class Monitor(object):
+    def __init__(self,espacio):
+        self.espacio = espacio #capacidad del restaurante
+        self.mutex = threading.Lock()
+        self.recepcion = threading.Condition(self.mutex)
+        self.clientes = threading.Condition(self.mutex)
+        self.mesero = threading.Condition(self.mutex)
+        self.cocinero = threading.Condition(self.mutex)
+        self.reservaciones = []
+        self.num_clientes = [] #numero de clientes dentro del restaurante
+        self.cola_espera = [] #clientes en espera para entrar al restaurante
+        self.ordenes = []
+        self.comida = []
+    #Recepcionista
+    def puedo_reservar(self):
+        with self.mutex:
+            if (self.reservaciones < RESERVACION_MAX):
+                return True
+            else:
+                return False
 
-RESERVACIONES
-- Se pueden hacer reservaciones de manera aleatoria para un máximo del 20% de la capacidad
-- Los hilos con reservación pasan a este proceso, se bloquean, tras cierto tiempo se desbloquean y llegan al restaurante
-- Cuando un hilo trate de hacer una reservación y no haya espacio se va a la cola del restaurante
+    def reservar(self,comensal):
+        with self.mutex:
+            if self.puedo_reservar():
+                print(f"Cliente {comensal.id} hizo una reservación")
+                self.reservaciones.append(comensal)
+                time.sleep(1)
+                self.entrar(comensal)
+            else:
+                self.encolar(comensal)
 
-COMENSAL
-- Llama a un mesero una vez entra al restaurante
-- Cambia la orden a EN PROCESO
-- Tardan un tiempo específico comiendo y al terminar se retiran y despiertan a los comensales en espera
+    def encolar(self,comensal):
+        with self.mutex:
+            self.cola_espera.append(comensal)
+            self.entrar(comensal)
+            if len(self.num_clientes) < self.espacio:
+                print(f"Cliente {comensal.id} llega al restaurante")
+                self.entrar(comensal)
+            else:
+                print(f"Cliente {comensal.id} se formó en la cola")
+                time.sleep(3)
+    #Comensal
+    def entrar(self,comensal):
+        self.recepcion.acquire()
+        if len(self.reservaciones) > 0 or len(self.cola_espera) > 0:
+            self.clientes.acquire()
+            print(f"Cliente {comensal.id} entra al restaurante")
+            if len(self.num_clientes) < self.espacio:
+                self.num_clientes.append(comensal)
 
-MESERO
-- Solo puede atender a un comensal simultaneamente
-- Genera una ORDEN que tiene 2 estados
-    EN PROCESO: el comensal bloquea al mesero y el mesero añade la orden a un buffer de ordenes infinito
-    LISTO: el cocinero desbloquea al mesero y añade a un buffer de comidas
-- Si no hay comensales todos los meseros descansan
+                self.mesero.acquire()
+                self.meseros.notify()
+                self.mesero.release()
+                self.clientes.release()
+            else:
+                print(f"Cliente {comensal.id} se formó en la cola")
+                self.clientes.wait()
 
-COCINERO
-- Esta en reposo si no hay ordenes en el buffer
-- Cocina, cambia la orden a LISTO y añade al buffer de comidas
-'''
+            self.recepcion.notify()
+            self.recepcion.release()
+        else:
+            self.recepcion.wait()
+    
+    def comer(self, comensal):
+        print(f"Cliente {comensal.id} está comiendo")
+        time.sleep(randint(1,5))
+        print(f"Cliente {comensal.id} terminó de comer")
+
+    def salir(self,comensal):
+        with self.mutex:
+            print(f"\nClientes: {len(self.num_clientes)}\n")
+            self.num_clientes.pop()
+            print(f"Cliente {comensal.id} ha salido")
+            time.sleep(1)
+            if len(self.cola_espera) > 0:
+                new_comensal = self.cola_espera.pop()
+                new_comensal.restaurant.entrar(new_comensal)
+            self.clientes.notify()
+    #Mesero
+    def descansar_mesero(self):
+        with self.mutex:
+            if len(self.num_clientes) > 0:
+                return False
+            else:
+                return True
+    
+    def crear_orden(self, mesero):
+        with self.mutex:
+            plato = Orden()
+            print(f"Mesero {mesero} tomo la orden del nuevo cliente que comerá {plato.food}")
+            time.sleep(1)
+            self.ordenes.append(plato)
+            self.cocineros.notify()
+    #Cocinero
+    def ordenes_pendientes(self):
+        with self.mutex:
+            if len(self.ordenes) > 0:
+                return True
+            else:
+                return False
+    def cocinar(self,id):
+        with self.mutex:
+            print(f"Cocinero {id} está cocinando")
+            orden = self.ordenes.pop()
+            time.sleep(1)
+            self.comida.append(orden)
+            print(f"Cocinero {id} preparó: {orden.food}")
+            self.liberar_orden()
+    def liberar_orden(self):
+        orden = self.comida.pop()
+        print(f"El mesero ha servido un platillo de {orden.food}")
+
+class Orden(object):
+    foods = ["spaguetti","lasagna","quesadilla","hamburguesa","huevos al gusto","tacos"]
+    def __init__(self): 
+        self.food = choice(Orden.foods)
+
+class Comensal(threading.Thread):
+    def __init__(self,id,monitor):
+        threading.Thread.__init__(self)
+        self.id = id
+        self.restaurant = monitor
+    def run(self):
+        reserva = randint(0,1)
+        if reserva == 1: 
+            self.restaurant.reservar(self) 
+        if reserva == 0:
+            self.restaurant.encolar(self)
+        self.restaurant.comer(self)
+
+class Mesero(threading.Thread):
+    def __init__(self,id,monitor):
+        threading.Thread.__init__(self)
+        self.id = id
+        self.restaurant = monitor
+
+    def run(self):
+        while self.restaurant.descansar_mesero():
+            print(f"Mesero {self.id} esta descansando")
+        self.restaurant.crear_orden(self.id)
+
+class Cocinero(threading.Thread):
+    def __init__(self,id,monitor):
+        threading.Thread.__init__(self)
+        self.id = id
+        self.restaurant = monitor
+    
+    def run(self):
+        while(self.restaurant.ordenes_pendientes()):
+            self.restaurant.cocinar(self.id)
+        print(f"Cocinero {self.id} esta esperando nuevas ordenes")            
+
+def main():
+    threads = []
+    restaurant = Monitor(CAPACIDAD)
+
+    for x in range(COMENSALES):
+        threads.append(Comensal(x+1,restaurant))
+
+    for x in range(MESEROS):
+        threads.append(Mesero(x+1,restaurant))
+
+    for x in range(COCINEROS):
+        threads.append(Cocinero(x+1,restaurant))
+
+    for t in threads:
+        t.start()
+
+    for t in threads:
+        t.join()
+
+if __name__ == "__main__":
+    main()
