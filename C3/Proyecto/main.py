eb2c75215162efa12739775cefba9e2775f6ad37
Gabriel <gabrielramondev23@gmail.com>
2022-11-23 18:56:56 -0600

    Avances en el juego

--- C3/Proyecto/main.py
+++ C3/Proyecto/main.py
@@ -1,5 +1,5 @@
-import pygame, sys
-from sprites_tropa import Archer, Knight, Lancer, Dragon
+import pygame, sys, time
+from sprites_tropa import Troop
 pygame.init()
 
 #color
@@ -9,39 +9,110 @@ H = 400
 size = (W,H)
 fps = 8
 screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Tower Defense")
 clock = pygame.time.Clock()
 
 all_sprite_list = pygame.sprite.Group()
-my_group = pygame.sprite.Group()
+red_team = pygame.sprite.Group()
+blue_team = pygame.sprite.Group()
 
-def invocar_archer():
-    archer = Archer("red")
-    my_group.add(archer) 
-def invocar_knight():
-    knight = Knight("red")
-    my_group.add(knight) 
-def invocar_lancer():
-    lancer = Lancer("red")
-    my_group.add(lancer) 
-def invocar_dragon():
-    dragon = Dragon("red")
-    my_group.add(dragon) 
+def summon_troop(troop_type,team):
+    troop = Troop(troop_type,team)
+    if team == "red":
+        red_team.add(troop)
+    else:
+        blue_team.add(troop)
 
-while True:
+intervalo = 1000
+run = True
+while run:
+    screen.fill(BG)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            sys.exit()
-        if event.type == pygame.KEYDOWN:
+            run = False
+        if event.type == pygame.KEYDOWN and pygame.time.get_ticks()/intervalo >= 1:
             if event.key == pygame.K_q:
-                invocar_archer()
+                summon_troop("archer","red")
             if event.key == pygame.K_w:
-                invocar_knight()
+                summon_troop("knight","red")
             if event.key == pygame.K_e:
-                invocar_lancer()
+                summon_troop("lancer","red")
             if event.key == pygame.K_r:
-                invocar_dragon()
-    my_group.update()
-    screen.fill(BG)
-    my_group.draw(screen)
+                summon_troop("dragon","red")
+            if event.key == pygame.K_u:
+                summon_troop("archer","blue")
+            if event.key == pygame.K_i:
+                summon_troop("knight","blue")
+            if event.key == pygame.K_o:
+                summon_troop("lancer","blue")
+            if event.key == pygame.K_p:
+                summon_troop("dragon","blue")
+            intervalo += 1000
+
+    red_team.update()
+    blue_team.update()
+    red_team.draw(screen)
+    blue_team.draw(screen)
+
+    list_red_troops = red_team.sprites()
+    #Formarlos en una fila
+    for troop_red in red_team:
+        if troop_red == red_team.sprites()[0]:
+            if troop_red.rect.x == (W/2)-60:
+                troop_red.speed = 0
+
+    for troop_blue in blue_team:
+        if troop_blue.rect.x == (W/2):
+            troop_blue.speed = 0
+
+    hits = pygame.sprite.groupcollide(blue_team,red_team,False,False)
+    for hit in hits:
+        hit.attacking = True
+        hits[hit][0].attacking = True
+        #damage done by blue troop
+        if hit.hp > 0:
+            if hit.type == "archer":
+                if hits[hit][0].type == "lancer":
+                    hits[hit][0].damage_taken = 8
+                else:
+                    hits[hit][0].damage_taken = 4
+            if hit.type == "knight":
+                if hits[hit][0].type == "archer":
+                    hits[hit][0].damage_taken = 6
+                else:
+                    hits[hit][0].damage_taken = 3
+            if hit.type == "lancer":
+                if hits[hit][0].type == "knight":
+                    hits[hit][0].damage_taken = 6
+                else:
+                    hits[hit][0].damage_taken = 3
+            if hit.type == "dragon":    
+                hits[hit][0].damage_taken = 12
+        else:
+            hits[hit][0].damage_taken = 0
+            hits[hit][0].speed = hits[hit][0].speed_initial
+        #damage done by red troop
+        if hits[hit][0].hp > 0:
+            if hits[hit][0].type == "archer":
+                if hit.type == "lancer":
+                    hit.damage_taken = 8
+                else:
+                    hit.damage_taken = 4
+            if hits[hit][0].type == "knight":
+                if hit.type == "archer":
+                    hit.damage_taken = 6
+                else:
+                    hit.damage_taken = 3
+            if hits[hit][0].type == "lancer":
+                if hit.type == "knight":
+                    hit.damage_taken = 6
+                else:
+                    hit.damage_taken = 3
+            if hits[hit][0].type == "dragon":    
+                hit.damage_taken = 12
+        else:
+            hit.damage_taken = 0
+            hit.speed = hit.speed_initial
+
     pygame.display.update()
     clock.tick(fps)
